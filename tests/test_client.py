aa2f2720f0270c3634f6c14601dd4c2e09f95bd3
An Long <aisk1988@gmail.com>
2021-01-31 21:14:09 +0800

    ✅ test on clinet directly

--- tests/test_client.py
+++ tests/test_client.py
@@ -4,22 +4,22 @@ import memcache
 
 
 @pytest.fixture()
-def connection():
-    return memcache.Connection("localhost", 12345)
+def client():
+    return memcache.Memcache("memcache://localhost:12345")
 
 
-def test_hello(connection):
+def test_hello(client):
     command = memcache.MetaCommand(cm=b"ms", key=b"foo", flags=[b"S3"], value=b"bar")
-    result = connection.execute_meta_command(command)
+    result = client.execute_meta_command(command)
     assert result.rc == b"OK"
 
     command = memcache.MetaCommand(cm=b"mg", key=b"foo", flags=[b"v"], value=None)
-    result = connection.execute_meta_command(command)
+    result = client.execute_meta_command(command)
     assert result.rc == b"VA"
     assert result.value == b"bar"
-    connection.close()
+    client.connection.close()
 
 
-def test_flush_all(connection):
-    connection.flush_all()
-    connection.flush_all()
+def test_flush_all(client):
+    client.flush_all()
+    client.flush_all()
