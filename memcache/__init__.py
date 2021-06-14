c7b42ad1b6f48c131a63d5ea8494d29af4aaa19a
An Long <aisk1988@gmail.com>
2021-06-14 22:52:18 +0800

    ✨ support asyncio

--- memcache/__init__.py
+++ memcache/__init__.py
@@ -1,6 +1,8 @@
+from .async_memcache import AsyncMemcache
 from .memcache import Memcache, MetaCommand, MetaResult, MemcacheError
 
 __all__ = [
+    "AsyncMemcache",
     "Memcache",
     "MemcacheError",
     "MetaResult",
