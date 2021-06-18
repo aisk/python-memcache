54bf060244c1b7d2813529d6c440aaf182eeae75
An Long <aisk1988@gmail.com>
2021-06-18 23:59:23 +0800

    💄 change to rtd theme for docs

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,8 @@ author = 'An Long'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc'
+    'sphinx.ext.autodoc',
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -45,24 +46,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_typlog_theme'
-
-html_theme_options = {
-    'logo': 'memcached.png',
-    'color': '#E8371A',
-    'description': 'Python client for memcached',
-    'github_user': 'aisk',
-    'github_repo': 'memcache',
-    'meta_html': '<meta name="generator" content="sphinx">',
-}
-
-html_sidebars = {
-    '**': [
-        'logo.html',
-        'github.html',
-        'globaltoc.html',
-    ]
-}
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
