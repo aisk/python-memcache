4b625faf605c1cb42f75e0302934ba7cd05e8703
AN Long <aisk1988@gmail.com>
2022-02-05 23:37:42 +0800

    ✨ support connection pool for sync client

--- memcache/memcache.py
+++ memcache/memcache.py
@@ -1,5 +1,8 @@
 import socket
-from typing import Any, List, Optional, Tuple, Union
+import threading
+import queue
+from contextlib import contextmanager
+from typing import Any, Callable, Iterator, List, Optional, Tuple, Union
 
 import hashring
 
@@ -99,43 +102,99 @@ class Connection:
 Addr = Tuple[str, int]
 
 
+class Pool:
+    def __init__(
+        self,
+        create_connection: Callable[..., Connection],
+        max_size: Optional[int],
+        timeout: Optional[int],
+    ) -> None:
+        self._create_connection = create_connection
+        self._max_size = max_size
+        self._timeout = timeout
+        self._size = 0
+        self._lock = threading.Lock()
+        self._connections: queue.Queue[Connection] = queue.Queue()
+
+    @contextmanager
+    def get(self) -> Iterator[Connection]:
+        try:
+            connection = self._connections.get_nowait()
+            yield connection
+            self._connections.put(connection)
+        except queue.Empty:
+            if self._max_size and self._size >= self._max_size:
+                connection = self._connections.get(timeout=self._timeout)
+                yield connection
+                self._connections.put(connection)
+            else:
+                with self._lock:
+                    self._size += 1
+                connection = self._create_connection()
+                yield connection
+                self._connections.put(connection)
+
+
 class Memcache:
     def __init__(
         self,
         addr: Union[Addr, List[Addr], None] = None,
         *,
+        pool_size: Optional[int] = 23,
+        pool_timeout: Optional[int] = 1,
         load_func: LoadFunc = load,
         dump_func: DumpFunc = dump
     ):
         addr = addr or ("localhost", 11211)
         if isinstance(addr, list):
+            addrs: List[Addr] = addr
+            nodes: List[Pool] = []
+            for addr in addrs:
+                create_connection = lambda: Connection(
+                    addr, load_func=load_func, dump_func=dump_func
+                )
+                nodes.append(
+                    Pool(create_connection, max_size=pool_size, timeout=pool_timeout)
+                )
+            self._connections = hashring.HashRing(nodes)
+        elif isinstance(addr, tuple):
+            a: Addr = addr
+            create_connection = lambda: Connection(
+                a, load_func=load_func, dump_func=dump_func
+            )
             self._connections = hashring.HashRing(
-                [Connection(x, load_func=load_func, dump_func=dump_func) for x in addr]
+                [Pool(create_connection, max_size=pool_size, timeout=pool_timeout)]
             )
         else:
-            self._connections = hashring.HashRing(
-                [Connection(addr, load_func=load_func, dump_func=dump_func)]
-            )
+            raise TypeError("invalid type for addr")
 
-    def _get_connection(self, key: Union[str, bytes]) -> Connection:
+    @contextmanager
+    def _get_connection(self, key: Union[str, bytes]) -> Iterator[Connection]:
         if isinstance(key, bytes):
             key = key.decode("utf-8")
-        return self._connections.get_node(key)  # type: ignore[no-any-return]
+        pool = self._connections.get_node(key)
+        with pool.get() as connection:
+            yield connection
 
     def execute_meta_command(self, command: MetaCommand) -> MetaResult:
-        return self._get_connection(command.key).execute_meta_command(command)
+        with self._get_connection(command.key) as connection:
+            return connection.execute_meta_command(command)
 
     def flush_all(self) -> None:
-        for connection in self._connections.nodes:
-            connection.flush_all()
+        for pool in self._connections.nodes:
+            with pool.get() as connection:
+                connection.flush_all()
 
     def set(
         self, key: Union[bytes, str], value: Any, *, expire: Optional[int] = None
     ) -> None:
-        return self._get_connection(key).set(key, value, expire=expire)
+        with self._get_connection(key) as connection:
+            return connection.set(key, value, expire=expire)
 
     def get(self, key: Union[bytes, str]) -> Optional[Any]:
-        return self._get_connection(key).get(key)
+        with self._get_connection(key) as connection:
+            return connection.get(key)
 
     def delete(self, key: Union[bytes, str]) -> None:
-        return self._get_connection(key).delete(key)
+        with self._get_connection(key) as connection:
+            return connection.delete(key)
